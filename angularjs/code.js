827f65db657415244962283eb034fa60e4e42d69
Jackson Sá <jacksonwendel@gmail.com>
2021-09-11 22:24:24 -0300

    Projeto init

---
+++ angularjs/code.js
@@ -0,0 +1,21 @@
+let a = "A piece of cake";
+        let b = a.slice(-10, -3);
+        console
+        app = angular.module("listaTelefonica", []);
+        app.controller("listaTelefonicaCtrl", function ($scope) {
+            $scope.titulo = "Lista Telefônica";
+
+            $scope.contatos = [{ nome: "Alisson", telefone: "123456" },
+            { nome: "Gabi", telefone: "987654" },
+            { nome: "Gustavo", telefone: "564789" },
+            { nome: "Pedro", telefone: "564789" }];
+
+            $scope.operadoras = ["Vivo","Claro","Tim","Oi"];
+
+            $scope.adicionarContato = function (item) {
+                $scope.contatos.push(item);
+                delete $scope.item; //deleta objeto do escopo
+                document.querySelector('.in1').value = null;
+                document.querySelector('.in2').value = null;
+            }
+        });
