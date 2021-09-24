3b785ddcd57cdbbe32ca763ff3643aacd8837fa7
Jackson Sá <jacksonwendel@gmail.com>
2021-09-24 19:09:57 -0300

    Atualizando aula
    Inserção de mais dados na lista telefônica

--- js/code.js
+++ js/code.js
@@ -1,18 +1,14 @@
 app = angular.module("listaTelefonica", []);
+
 app.controller("listaTelefonicaCtrl", function ($scope) {
     $scope.titulo = "Lista Telefônica";
 
-    $scope.contatos = [{ nome: "Alisson", telefone: "123456" },
-    { nome: "Gabi", telefone: "987654" },
-    { nome: "Gustavo", telefone: "564789" },
-    { nome: "Pedro", telefone: "564789" }];
+    $scope.contatos = [];
 
     $scope.operadoras = ["Vivo","Claro","Tim","Oi"];
 
     $scope.adicionarContato = function (item) {
         $scope.contatos.push(item);
         delete $scope.item; //deleta objeto do escopo
-        document.querySelector('.in1').value = null;
-        document.querySelector('.in2').value = null;
     }
 });
